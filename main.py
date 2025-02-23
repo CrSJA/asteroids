9475e3e6c426ebcfe0b7e118c08145c740ae3c3f
Aurelian <u.picadov@gmail.com>
2025-02-23 16:35:42 -0600

    add circleshape

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame as pg
 from constants import *
 def main():
+
     pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clok = pg.time.Clock()
+    dt=0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            
+        
+
+
+
         pg.display.flip()
+        dt = (clok.tick(60))/1000
 
 
 if __name__ =="__main__":
