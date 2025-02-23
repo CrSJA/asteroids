4c9afce96f1dd995e050348a973cfa6f8ad40e89
Aurelian <u.picadov@gmail.com>
2025-02-22 19:44:34 -0600

    add loob to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pygame 
-
+import pygame as pg
+from constants import *
 def main():
-    print("asteroids")
+    pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        pg.display.flip()
 
 
 if __name__ =="__main__":
