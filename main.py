37deefbdedf347d015b8635bb1f802d4e3b02992
Aurelian <u.picadov@gmail.com>
2025-02-22 19:49:55 -0600

    made it so quit works

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 def main():
     pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
         pg.display.flip()
 
 
