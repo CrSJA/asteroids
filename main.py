8f0c7f6498d78de0594815fee27c9094811f1a1a
Aurelian <u.picadov@gmail.com>
2025-02-24 17:16:07 -0600

    add movement

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame as pg
 from constants import *
 from player import *
+
 def main():
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clok =  pg.time.Clock()
     dt=0
 
-    play=player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    playe=player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
 
 
     while True:
+        screen.fill(("black"))
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
             
         
+        playe.update(dt)
 
-
-        play.draw(screen)
+        playe.draw(screen)
         pg.display.flip()
         dt = (clok.tick(60))/1000
 
